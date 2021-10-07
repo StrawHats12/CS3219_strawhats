fa373735a0bd6ddfecedb065c63e104b18fabeb0
Samuel Fang <samuelfangjw@gmail.com>
2021-10-07 13:44:35 +0800

    Add placeholder image for listings

--- frontend/src/components/Listings/ListingsCard.jsx
+++ frontend/src/components/Listings/ListingsCard.jsx
@@ -2,15 +2,16 @@ import Storage from "@aws-amplify/storage";
 import { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import placeholder from "./placeholder-image.jpg";
 
 const ListingsCard = (props) => {
   const listing = props.listing;
   const { id, listing_name, description, images, seller_uid } = listing || {};
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState(placeholder);
 
   useEffect(() => {
     async function getImage() {
-      if (images) {
+      if (images && images.length) {
         const image = await Storage.get(images[0], {
           level: "protected",
           identityId: seller_uid,
