b9ba6dadee1b1a2e7bc28507ab0355681676af96
Samuel Fang <samuelfangjw@gmail.com>
2021-10-29 19:58:15 +0800

    Add error boundary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,42 +17,44 @@ import Authentication from "./pages/Authentication";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import { ProfilePage } from "./pages/Profile";
 import Livestream from "./pages/Livestream";
-
+import ErrorBoundary from "./utils/ErrorBoundary";
 
 function App() {
   return (
-    <Router>
-      <div className="min-vh-100 d-flex flex-column">
-        <NavBar />
-        <Container>
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/profile/:username" component={ProfilePage} exact />
-            <Route path="/listings" component={ListingsPage} exact />
-            <ProtectedRoute
-              path="/listings/create"
-              component={CreateListingPage}
-              exact
-            />
-            <ProtectedRoute
-              path="/listings/edit/:id"
-              component={EditListingPage}
-              exact
-            />
-            <Route path="/listings/:id" component={ListingPage} exact />
-            <ProtectedRoute
-              path="/tv/:streamerId"
-              component={Livestream}
-              exact
-            />
-            <Route path="/authentication" component={Authentication} exact />
-            <ProtectedRoute path="/messenger" component={Messenger} exact />
-            <Route component={NotFound} />
-          </Switch>
-        </Container>
-        <Footer />
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <div className="min-vh-100 d-flex flex-column">
+          <NavBar />
+          <Container>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/profile/:username" component={ProfilePage} exact />
+              <Route path="/listings" component={ListingsPage} exact />
+              <ProtectedRoute
+                path="/listings/create"
+                component={CreateListingPage}
+                exact
+              />
+              <ProtectedRoute
+                path="/listings/edit/:id"
+                component={EditListingPage}
+                exact
+              />
+              <Route path="/listings/:id" component={ListingPage} exact />
+              <ProtectedRoute
+                path="/tv/:streamerId"
+                component={Livestream}
+                exact
+              />
+              <Route path="/authentication" component={Authentication} exact />
+              <ProtectedRoute path="/messenger" component={Messenger} exact />
+              <Route component={NotFound} />
+            </Switch>
+          </Container>
+          <Footer />
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
---
+++ frontend/src/utils/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Container className="mx-auto my-4 text-center">
+          <h2>
+            Something went wrong. We are sorry. Please refresh the page and
+            contact the system administrator if the issue persists.
+          </h2>
+        </Container>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
